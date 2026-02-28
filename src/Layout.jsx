8ef333dff92dc8810054b4ea07cf4c9a8b3aaa1f
brunorochamczal <brunorocha81@gmail.com>
2026-02-28 15:51:25 -0300

    Update Layout.jsx

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { createPageUrl } from "./utils";
+import { createPageUrl } from "@/lib/utils";
 import { aroma } from "@/api/aromaClient";
 import { 
   LayoutDashboard, 
