40f1ebed0b901a3f7f249fc627b356cb43b6b990
brunorochamczal <brunorocha81@gmail.com>
2026-02-28 21:14:51 -0300

    Update Fornecedores.jsx

--- src/pages/Fornecedores.jsx
+++ src/pages/Fornecedores.jsx
@@ -192,7 +192,7 @@ export default function Fornecedores() {
                         className="text-red-600"
                       >
                         <Trash2 className="h-4 w-4 mr-2" />
-                        Desativar
+                        Excluir
                       </DropdownMenuItem>
                     </DropdownMenuContent>
                   </DropdownMenu>
